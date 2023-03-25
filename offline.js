9c9696d44f19a82785f29b31cb6427e3a8d991c5
nel-feelingood <128934749+nel-feelingood@users.noreply.github.com>
2023-03-26 01:43:11 +0400

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1679780087,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/ground.png",
+		"images/base-sheet0.png",
+		"images/megaman-sheet0.png",
+		"images/megaman-sheet1.png",
+		"images/ladder.png",
+		"images/spring-sheet0.png",
+		"images/spring-sheet1.png",
+		"images/tapjump-sheet0.png",
+		"images/tapleft-sheet0.png",
+		"images/tapright-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
